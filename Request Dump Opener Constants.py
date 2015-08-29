2c152f2b7585e383678e69d138456d53b7e50aed
David Skudra <dskudra@gmail.com>
2015-08-29 11:09:58 -0400

    Added basic constants for opening provided jsons
    Make sure to fill in the directories.

---
+++ Request Dump Opener Constants.py
@@ -0,0 +1,83 @@
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\BR.json') as data_file:
+        BR511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\EUNE.json') as data_file:
+        EUNE511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\EUW.json') as data_file:
+        EUW511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\KR.json') as data_file:
+        KR511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\LAN.json') as data_file:
+        LAN511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\LAS.json') as data_file:
+        LAS511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\NA.json') as data_file:
+        NA511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\OCE.json') as data_file:
+        OCE511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\RU.json') as data_file:
+        RU511N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\NORMAL_5X5\TR.json') as data_file:
+        TR511N = json.load(data_file)
+        
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\BR.json') as data_file:
+        BR511R = json.load(data_file) 
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\EUNE.json') as data_file:
+        EUNE511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\EUW.json') as data_file:
+        EUW511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\KR.json') as data_file:
+        KR511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\LAN.json') as data_file:
+        LAN511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\LAS.json') as data_file:
+        LAS511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\NA.json') as data_file:
+        NA511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\OCE.json') as data_file:
+        OCE511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\RU.json') as data_file:
+        RU511R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.11\RANKED_SOLO\TR.json') as data_file:
+        TR511R = json.load(data_file)
+        
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\BR.json') as data_file:
+        BR514N = json.load(data_file) 
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\EUNE.json') as data_file:
+        EUNE514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\EUW.json') as data_file:
+        EUW514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\KR.json') as data_file:
+        KR514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\LAN.json') as data_file:
+        LAN514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\LAS.json') as data_file:
+        LAS514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\NA.json') as data_file:
+        NA514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\OCE.json') as data_file:
+        OCE514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\RU.json') as data_file:
+        RU514N = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\NORMAL_5X5\TR.json') as data_file:
+        TR514N = json.load(data_file)        
+        
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\BR.json') as data_file:
+        BR514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\EUNE.json') as data_file:
+        EUNE514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\EUW.json') as data_file:
+        EUW514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\KR.json') as data_file:
+        KR514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\LAN.json') as data_file:
+        LAN514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\LAS.json') as data_file:
+        LAS514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\NA.json') as data_file:
+        NA514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\OCE.json') as data_file:
+        OCE514R = json.load(data_file)
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\RU.json') as data_file:
+        RU514R = json.load(data_file)   
+with open(r'E:\DIRECTORY HERE\5.14\RANKED_SOLO\TR.json') as data_file:
+        TR514R = json.load(data_file)
